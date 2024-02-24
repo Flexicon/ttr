f150d67dca4a5c683a5c11a770423bd9cb0afb76
Mike Repec <mikerepec@gmail.com>
2024-02-24 13:14:21 +0100

    Set up rank voting

--- src/components/RankingBoard.tsx
+++ src/components/RankingBoard.tsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import cn from 'classnames';
+
 import type { Album } from '../types/Album';
 
 export type AlbumBoardProps = {
@@ -5,15 +8,51 @@ export type AlbumBoardProps = {
 };
 
 export const AlbumBoard = (props: AlbumBoardProps) => {
+  const [ranking, setRanking] = useState<string[]>([]);
+
+  const rankTrack = (track: string) => {
+    setRanking((ranking) => {
+      if (ranking.includes(track)) {
+        return ranking.filter((t) => t !== track);
+      }
+      return [...ranking, track];
+    });
+  };
+
+  const sortByRanking = (a: string, b: string): number => {
+    const aIdx = ranking.indexOf(a);
+    const bIdx = ranking.indexOf(b);
+
+    if (aIdx === -1 && bIdx === -1) return 0;
+    if (aIdx === -1) return 1;
+    if (bIdx === -1) return -1;
+    return aIdx < bIdx ? 1 : -1;
+  };
+
   return (
-    <div className="py-4 px-8 mb-4">
+    <div>
       <img className="max-w-full w-[320px] mb-4" src={`/${props.album.cover}`} title={props.album.title} />
       <ul className="w-[320px] max-w-full">
-        {props.album.tracklist.map((track) => (
-          <li className="px-2 py-1 mb-3 text-sm cursor-pointer rounded border border-pink-300 bg-pink-200 hover:bg-pink-300">
-            {track}
-          </li>
-        ))}
+        {props.album.tracklist.sort(sortByRanking).map((track) =>
+          ranking.length < 3 || ranking.includes(track) ? (
+            <li
+              key={track}
+              className={cn(
+                'px-2 py-1 mb-3 text-sm cursor-pointer rounded border border-pink-300 bg-pink-200 hover:bg-pink-300 transition-colors',
+                ranking.indexOf(track) === 0 && 'bg-amber-800 hover:bg-amber-900 border-amber-900 text-white',
+                ranking.indexOf(track) === 1 && 'bg-stone-100 hover:bg-stone-300 border-stone-300',
+                ranking.indexOf(track) === 2 && 'bg-yellow-300 hover:bg-yellow-400 border-yellow-400'
+              )}
+              onClick={() => rankTrack(track)}
+              role="button"
+            >
+              {ranking.indexOf(track) === 0 && '🥉 '}
+              {ranking.indexOf(track) === 1 && '🥈 '}
+              {ranking.indexOf(track) === 2 && '🥇 '}
+              {track}
+            </li>
+          ) : null
+        )}
       </ul>
     </div>
   );
@@ -25,7 +64,7 @@ export type RankingBoardProps = {
 
 export function RankingBoard(props: RankingBoardProps) {
   return (
-    <div>
+    <div className="flex px-8 py-4 gap-8">
       {props.albums.map((album) => (
         <AlbumBoard album={album} />
       ))}
